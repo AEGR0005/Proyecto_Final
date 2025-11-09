59c762694f941fba45602cc41286ef874707e4de
nilse <nilse@LenovoLOQ>
2025-11-09 13:06:05 -0400

    atributos

--- ProyectoFinal/src/logico/Doctor.java
+++ ProyectoFinal/src/logico/Doctor.java
@@ -3,6 +3,12 @@ package logico;
 import java.util.ArrayList;
 
 public class Doctor {
+	
+	private String idDoctor;
+	private String nombre;
+	private int cupoDiario;
+	
+	
 	private ArrayList<HorarioDisponible> horariosDisponibles;
 	// IMPORTANTE UTILIZAR ESTE ATRIBUTO!!!!!!!!
 	
