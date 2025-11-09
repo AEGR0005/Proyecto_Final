e402caee32da35f8cc53a10742ea63c0649e9b57
nilse <nilse@LenovoLOQ>
2025-11-09 12:39:20 -0400

    Atributos paciente

--- ProyectoFinal/src/logico/Paciente.java
+++ ProyectoFinal/src/logico/Paciente.java
@@ -1,6 +1,16 @@
 package logico;
 
+import java.util.ArrayList;
+
 public class Paciente {
-	int a;
-	int b;
+	
+	private String idPaciente;
+	private String nombre;
+	private String cedula;
+	private String telefono;
+	private String fecNacim;
+	
+	
+	private ArrayList<Consulta> historialCOnsulta;
+	
 }
